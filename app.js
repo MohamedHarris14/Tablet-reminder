c394446bf28a3a1f590217fc65d0ef99a7679807
Mohamed Harris <110349074+MohamedHarris14@users.noreply.github.com>
2025-09-14 13:06:02 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -5,21 +5,21 @@ const cron = require("node-cron");
 
 const app = express();
 const PORT = 8010;
-
+require("dotenv").config();
 // Configure your email transporter (use Gmail or any SMTP service)
 const transporter = nodemailer.createTransport({
   service: "gmail", // or "hotmail", "yahoo", etc.
   auth: {
-    user: "ahamedharris14@gmail.com",   // replace with your email
-    pass: "dvrzykyl qjaq xjuq"       // use App Password, not real password
+    user: process.env.EMAIL_USER,   // replace with your email
+    pass: process.env.EMAIL_PASS       // use App Password, not real password
   }
 });
 
 // Function to send reminder mail
 function sendReminder(time) {
   const mailOptions = {
-    from: "ahamedharris14@gmail.com",
-    to: "ahamedharris14@gmail.com",  // replace with recipient email
+    from: process.env.EMAIL_USER,
+    to: process.env.EMAIL_To,
     subject: "Tablet Reminder",
     text: `It's ${time}! tablet 💊 poda maranthuratha pondatti 😻 😘.`
   };
@@ -33,26 +33,13 @@ function sendReminder(time) {
   });
 }
 
-// Schedule jobs
-// Morning 9:00
-cron.schedule("0 9 * * *", () => {
-  sendReminder("Morning 9:00 AM");
-}, { timezone: "Asia/Kolkata" });
 
-// Evening 9:00
-cron.schedule("0 21 * * *", () => {
-  sendReminder("Evening 9:00 PM");
-}, { timezone: "Asia/Kolkata" });
+const now = new Date();
 
-
-app.get("/", (req, res) => {
-  res.send("Tablet Reminder Service is running...");
-});
-
-app.get("/test", async (req, res) => {
+app.get("/", async (req, res) => {
   try {
-    await sendReminder("Manual Test Reminder");
-    res.send("✅ Test email sent successfully EMAIL_TO!");
+    await sendReminder(now.toLocaleTimeString());
+    res.send("✅ Test email sent successfully!");
   } catch (err) {
     res.send("❌ Error sending email: " + err.message);
   }
@@ -62,5 +49,3 @@ app.get("/test", async (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
-
-
